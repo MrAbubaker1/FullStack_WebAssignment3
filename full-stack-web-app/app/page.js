b45a7af8d568c8c62b2a0a87d09159cf6b57de5c
Amadou <amadouamos@hotmail.com>
2024-03-23 23:44:28 -0600

    styling

--- full-stack-web-app/app/page.js
+++ full-stack-web-app/app/page.js
@@ -2,15 +2,10 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div
-      className="min-h-screen flex flex-col justify-center items-center"
-      style={{
-        backgroundImage: 'linear-gradient(to bottom, #f7fafc, #edf2f7)',
-      }}
-    >
-      <div className="max-w-3xl mx-auto px-6 py-8 rounded-md">
-        <h1 className="text-4xl font-bold text-center mb-8">Welcome to IMR</h1>
-        <p className="text-xl text-center mb-8">Your destination for renting movies online</p>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-300">
+      <div className="max-w-3xl mx-auto px-6 py-8 rounded-md ">
+        <h1 className="text-4xl font-bold text-center mb-8">Welcome to IMRs Movie Portal</h1>
+        <p className="text-xl text-center mb-8">Database Sign In</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <button className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded-md text-center">
             Log In
